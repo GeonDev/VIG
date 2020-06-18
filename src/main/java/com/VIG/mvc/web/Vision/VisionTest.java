3cacf1336ba4b95d1a7684542db19d1a0e81ec3b
SonGeon <cadajue@gmail.com>
2020-06-19 02:00:06 +0900

    임시 세팅

--- src/main/java/com/VIG/mvc/web/Vision/VisionTest.java
+++ src/main/java/com/VIG/mvc/web/Vision/VisionTest.java
@@ -3,7 +3,8 @@ package com.VIG.mvc.web.Vision;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.VIG.mvc.web.Translate.TranslateKeyword;
+import com.VIG.mvc.service.domain.ImageKeyword;
+import com.VIG.mvc.web.Translate.Translater;
 import com.google.api.services.translate.Translate;
 
 public class VisionTest {
@@ -14,10 +15,10 @@ public class VisionTest {
 			String imageFilePath = "C:\\workspace\\VIG\\src\\main\\java\\com\\VIG\\mvc\\web\\Vision\\face.jpg"; 			
 			
 			System.out.println("VISION START...");
-			List<ImageKeyword> keys = VisionKeyword.getKeywordForVision(imageFilePath);			
+			List<ImageKeyword> keys = VisionInfo.getKeywordForVision(imageFilePath);			
 			
 			for(ImageKeyword key : keys) {
-				System.out.println(key.getKeyword() +" score : " +key.getScore());
+				System.out.println(key.getKeywordEn() +" , "+key.getKeywordKr() + " score : " +key.getScore());
 			}		
 			System.out.println("VISION END...");
 			
@@ -29,7 +30,7 @@ public class VisionTest {
 			
 			System.out.println("Tanslate START...");
 			
-			System.out.println(TranslateKeyword.autoDetectTranslate("스마트폰", "en"));
+			System.out.println(Translater.autoDetectTranslate("스마트폰", "en"));
 			
 			
 			System.out.println("Tanslate END...");
--- src/main/java/com/VIG/mvc/web/test/TestController.java
+++ src/main/java/com/VIG/mvc/web/test/TestController.java
@@ -1,13 +1,32 @@
 package com.VIG.mvc.web.test;
 
+import java.io.File;
+import java.util.List;
+
+import javax.servlet.ServletContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+
+
 
 
 @Controller
 @RequestMapping("/VIG/testController/*")
 public class TestController {
+	
+	
+	@Value("#{commonProperties['uploadPath']}")
+	String uploadPath;
+	
+	@Autowired
+	private ServletContext context;
 
 	public TestController() {
 		// TODO Auto-generated constructor stub
@@ -23,9 +42,27 @@ public class TestController {
 	
 	
 	@RequestMapping(value = "upload", method = RequestMethod.POST)
-	public void updatefile() throws Exception {
+	public ModelAndView updatefile(@RequestParam("uploadFile") List<MultipartFile> files) throws Exception {
 		
-		System.out.println("일단 업로드에 정상 도착");
+        String path = context.getRealPath("/");        
+        path = path.substring(0,path.indexOf("\\.metadata"));         
+        path = path +  uploadPath;  
+		
+		if(files !=null) {
+	        for (MultipartFile multipartFile : files) {
+	        	//파일 업로드시 시간을 이용하여 이름이 중복되지 않게 한다.
+	        	String inDate   = new java.text.SimpleDateFormat("yyyyMMddHHmmss").format(new java.util.Date());
+	    
+	    		File f =new File(path+inDate+multipartFile.getOriginalFilename());
+	    		
+	    		//원하는 위치에 파일 저장
+	    		multipartFile.transferTo(f);	    		
+	    	
+	    			    	
+			} 			
+		}
+		
+		return new ModelAndView("forward:/common/alertView.jsp", "message", "임시기능.");
 	}
 	
 		
