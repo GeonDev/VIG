dc923d36cf3637039e1bb0565da1ae85921fa41f
SonGeon <cadajue@gmail.com>
2020-06-26 20:06:17 +0900

    코드 간소화

--- src/main/java/com/VIG/mvc/util/EchoHandler.java
+++ src/main/java/com/VIG/mvc/util/EchoHandler.java
@@ -1,9 +1,7 @@
 package com.VIG.mvc.util;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
@@ -22,67 +20,46 @@ public class EchoHandler extends TextWebSocketHandler{
     private List<WebSocketSession> sessionList = new ArrayList<WebSocketSession>();
     
     
-    /**
-     * 클라이언트 연결 이후에 실행되는 메소드
-     */
+    //클라이언트 연결후 진행
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         //맵을 쓸때 방법
     	//sessions.put(session.getId(), session);
-        //List쓸때 방법
-        sessionList.add(session);
-        //세션값을 불러온
-        //0번째 중괄호에 session.getId()을 넣으라는뜻
-       
+ 
+        sessionList.add(session);       
         
-        //Session값을 가지고 데이터베이스등의 작업을 하면 채팅 참여 사용자 정보 리스트를 구현할 수 있다.//
         User user = (User)session.getAttributes().get("user");
         System.out.println("채팅방 입장자: " + user.getName());
     }
     
-    /**
-     * 클라이언트가 웹소켓 서버로 메시지를 전송했을 때 실행되는 메소드
-     */
+    //클라이언트 메세지를 받고 보내기
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-        //0번째에 session.getId() 1번째에 message.getPayload() 넣음
-     
-        //logger.info("{}로부터 {}받음", new String[]{session.getId(),message.getPayload()});
-        
-        //연결된 모든 클라이언트에게 메시지 전송 : 리스트 방법
-        //getPrincipal()를 이용해서 세션에 물려있는 유저의 정보를 불러온다.세션의 정보는 User를 이용한것과 동일하다.//
+       
+        //연결된 모든 클라이언트에게 메시지 전송   
         for(WebSocketSession sess : sessionList){        	
         	
-        	String name =  ((User)sess.getAttributes().get("user")).getName();
+        	String name =  ((User)sess.getAttributes().get("user")).getName();        	    
         	
-        	sess.sendMessage(new TextMessage(name + "|" + message.getPayload()));
-        }
-        
-        // 맵 방법.
-        /*Iterator<String> sessionIds = sessions.ketSet().iterator();
-        String sessionId = "";
-        while (sessionIds.hasNext()) {
-            sessionId = sessionIds.next();
-            sessions.get(sessionId).sendMessage(new TextMessage("echo:" + message.getPayload()));
-            
-        }*/
-        
-        //연결되어 있는 모든 클라이언트들에게 메시지를 전송한다.
-        //session.sendMessage(new TextMessage("echo:" + message.getPayload()));
+        	//메세지를 전송한다. message.getPayload() => 저장되어 있는 텍스트를 불러옴
+        	sess.sendMessage(new TextMessage(name + " | " + message.getPayload()));
+        }        
+   
     }
     
-    /**
-     * 클라이언트 연결을 끊었을 때 실행되는 메소드
-     */
+    //클라이언트와 연결이 끊어졌을 때 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-        //List 삭제
+       
+    	String name =  ((User)session.getAttributes().get("user")).getName();
+    	
+    	//List 삭제
         sessionList.remove(session);
         
         //Map 삭제
-        //sessions.remove(session.getId());      
+        //sessions.remove(session.getId());              
         
-        System.out.println("채팅방 퇴장자: " + session.getPrincipal().getName());
+        System.out.println("채팅방 퇴장자: " + name);
     }
  
 }
