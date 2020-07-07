e46e33e8a2c8216a16fd7024e15ceb39418453dd
SonGeon <geons.dev@gmail.com>
2020-07-08 03:40:20 +0900

    협업자 테이블 생성 중

--- src/main/java/com/VIG/mvc/service/domain/Likes.java
+++ src/main/java/com/VIG/mvc/service/domain/Likes.java
@@ -1,6 +1,8 @@
 package com.VIG.mvc.service.domain;
 
 
+import java.sql.Date;
+
 import lombok.Data;
 
 @Data
@@ -9,6 +11,7 @@ public class Likes {
 	private int likeId;
 	private User likeedUser;	
 	private int feedId;
+	private Date likeDate;
 	
 	
 	public Likes() {
