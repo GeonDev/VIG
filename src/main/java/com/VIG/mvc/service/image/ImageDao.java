d9c237ddc744cde78c094f6b970111b2a1756b07
SonGeon <cadajue@gmail.com>
2020-07-15 03:47:21 +0900

    프로필 기본 세팅

--- src/main/java/com/VIG/mvc/service/image/ImageDao.java
+++ src/main/java/com/VIG/mvc/service/image/ImageDao.java
@@ -10,15 +10,17 @@ public interface ImageDao {
 	public void addImage(Image image) throws Exception;
 	
 	//특정 이미지 번호를 넣어 단일 조회
-	public Image getImage(int imageId) throws Exception;
+	public Image getImageOne(int imageId) throws Exception;
+	
+	//등록된 전체 이미지 받아오기 - 세팅용
+	public List<Image> getALLImageList() throws Exception;
 	
 	//검색조건을 넣어 이미지 리스트를 출력
 	public List<Image> getImageList(Search search) throws Exception;	
 	
 	//색상을 기준으로 조회
 	public List<Image> getImageListfromColor(Search search) throws Exception;
-	
-	
+		
 	public void deleteImage(String image) throws Exception;
 
 }
