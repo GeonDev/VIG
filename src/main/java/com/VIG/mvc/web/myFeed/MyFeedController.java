27331fda54ae9b8856cb46912dca14aa12e8f260
SonGeon <geons.dev@gmail.com>
2020-07-22 11:05:29 +0900

    [수하]: myFeedController 수정중 Temp Commit

--- src/main/java/com/VIG/mvc/web/myFeed/MyFeedController.java
+++ src/main/java/com/VIG/mvc/web/myFeed/MyFeedController.java
@@ -5,7 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.VIG.mvc.service.feed.FeedServices;
 
@@ -27,6 +31,18 @@ public class MyFeedController {
 	
 	
 	public MyFeedController(){
+	}
+	
+	@RequestMapping(value="getMyFeedList", method=RequestMethod.GET)
+	public ModelAndView getMyFeedList(@RequestParam("userCode") String userCode) throws Exception {
+		
+		System.out.println("getMyFeedList");
+		
+		
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("forward:../myFeed/myFeedListTest.jsp");
+		
+		return mav; 
 	}
 	
 	
