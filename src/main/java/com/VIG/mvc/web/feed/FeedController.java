7f4fc35f2c9894d079ea387e1ce28654ebba9a4f
Sungjin <tjdwls2775@gmail.com>
2020-07-24 18:32:18 +0900

    성진 피드,이미지 Mapper,이미지서비스,피드컨트롤러

--- src/main/java/com/VIG/mvc/web/feed/FeedController.java
+++ src/main/java/com/VIG/mvc/web/feed/FeedController.java
@@ -6,6 +6,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import javax.servlet.ServletContext;
+import javax.servlet.http.HttpSession;
 
 import org.apache.ibatis.javassist.compiler.ast.Keyword;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -24,6 +26,7 @@ import com.VIG.mvc.service.color.ColorServices;
 import com.VIG.mvc.service.domain.Category;
 import com.VIG.mvc.service.domain.Event;
 import com.VIG.mvc.service.domain.Feed;
+import com.VIG.mvc.service.domain.Image;
 import com.VIG.mvc.service.domain.ImageColor;
 import com.VIG.mvc.service.domain.ImageKeyword;
 import com.VIG.mvc.service.domain.JoinUser;
@@ -75,30 +78,13 @@ public class FeedController {
 	
 	
 	@RequestMapping(value = "addFeed", method = RequestMethod.POST)
-	public ModelAndView addFeed(@RequestParam("keyword") String keyword,@ModelAttribute("feed") Feed feed, @ModelAttribute("category") Category category,@RequestParam("uploadFile") List<MultipartFile> files) throws Exception {
-										
-         
-				String[] originKeyword = keyword.split(","); //스트링으로 받은 키워드를 파싱후 string[]에 담는다
+	public ModelAndView addFeed(@RequestParam("keyword") String keyword,@ModelAttribute("feed") Feed feed, @ModelAttribute("category") Category category,@RequestParam("uploadFile") List<MultipartFile> files, @SessionAttribute("user") User user) throws Exception {
 				
-				for(int i =0; i < originKeyword.length;  i++) {			
-				int imageId=30000;			//Test용 이미지아이디
-				ImageKeyword imageKeyword = new ImageKeyword();          
-				imageKeyword.setKeywordOrigin(originKeyword[i]);                //이미지키워드에 오리진키워드 set
-				imageKeyword.setImageId(imageId);
-		//		keywordServices.addKeyword(imageKeyword);                		  
-				System.out.println(imageKeyword);	
-				}
-									
-		
-		String userCode = "user11";
-		
-		User user = userServices.getUserOne(userCode);
 		feed.setWriter(user);									
-		feed.setFeedCategory(category);	
-				
-		System.out.println("category :"+category);
-		System.out.println("feed : "+feed);			
-		System.out.println("추가한 이미지 파일: ");
+		feed.setFeedCategory(category);			
+		feedServices.addFeed(feed);
+
+		System.out.println("feed : "+feed);						
 		
         String path = context.getRealPath("/");        
         path = path.substring(0,path.indexOf("\\.metadata"));         
@@ -109,13 +95,42 @@ public class FeedController {
         List<ImageColor> colors = new ArrayList<ImageColor>();
         
 		if(files !=null) {
+			
+			
 	        for (MultipartFile multipartFile : files) {
+	        	
 	        	//파일 업로드시 시간을 이용하여 이름이 중복되지 않게 한다.
 	        	String inDate   = new java.text.SimpleDateFormat("yyyyMMddHHmmss").format(new java.util.Date());
-	    
+	        	
 	    		File f =new File(path+inDate+multipartFile.getOriginalFilename());
 	    		//원하는 위치에 파일 저장
-	    		multipartFile.transferTo(f);	
+	    		multipartFile.transferTo(f);		    		
+	    		
+	    		Image image = new Image();
+	    		
+	    		String imageFile=inDate+multipartFile.getOriginalFilename();
+	    	    System.out.println("imageFile:"+imageFile);
+	    	    
+	    		int getfeedId = feedServices.getLastFeedId();				//마지막 피드아이디 = getfeedId
+	    		image.setFeedId(getfeedId);											//이미지에 피드ID,이미지파일 셋
+	    		image.setImageFile(imageFile);												
+	    		imageServices.addImage(image);												
+	    		
+	    		int imageId = imageServices.getLastImageId();				//마지막 이미지Id  = imageId
+	    		
+	    		
+				String[] originKeyword = keyword.split(","); //스트링으로 받은 키워드를 파싱후 string[]에 담는다
+	    									
+	    					for(int i =0; i < originKeyword.length;  i++) {			    			
+	    					
+	    					ImageKeyword imageKeyword = new ImageKeyword();          
+	    			     	imageKeyword.setKeywordOrigin(originKeyword[i]);                //이미지키워드에 오리진키워드 set
+	    					imageKeyword.setIsTag(1);
+	    					imageKeyword.setImageId(imageId);
+	    					keywordServices.addKeyword(imageKeyword);                		  
+	    					System.out.println(imageKeyword);	
+	    					}
+	    		System.out.println(image);
 	    		
 	    		//keys = VisionInfo.getKeywordForVision(path+inDate+multipartFile.getOriginalFilename());	    			
 	    		//colors = VisionInfo.getColorForVision(path+inDate+multipartFile.getOriginalFilename());
@@ -139,6 +154,7 @@ public class FeedController {
 			}
 			
 		}	
+		
 		
 		return new ModelAndView("forward:/common/alertView.jsp", "message", result);
 	}	
