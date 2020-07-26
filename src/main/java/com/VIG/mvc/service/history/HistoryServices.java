baeef86797031109afcb90fd6954e49378c4bb83
SonGeon <cadajue@gmail.com>
2020-07-27 03:42:14 +0900

    [손건] 이미지 색상 저장시 10 단위로 변경/ 히스토리 매핑 시작

---
+++ src/main/java/com/VIG/mvc/service/history/HistoryServices.java
@@ -0,0 +1,14 @@
+package com.VIG.mvc.service.history;
+
+import java.util.List;
+
+import com.VIG.mvc.service.domain.History;
+import com.VIG.mvc.service.domain.Search;
+
+public interface HistoryServices {
+	
+	public List<History> getHistoryList(Search search) throws Exception;
+	
+	public void addHistory(History history) throws Exception;
+
+}
