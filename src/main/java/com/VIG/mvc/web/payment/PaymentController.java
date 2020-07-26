3a1d94f14c5bdd2424b3ce727bb4868fe0adcb89
Sooha <svha0504@gmail.com>
2020-07-27 00:20:20 +0900

    [수하] : payment CRUD 작성 /  addPayment.jsp 생성

--- src/main/java/com/VIG/mvc/web/payment/PaymentController.java
+++ src/main/java/com/VIG/mvc/web/payment/PaymentController.java
@@ -1,5 +1,7 @@
 package com.VIG.mvc.web.payment;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
@@ -11,6 +13,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.VIG.mvc.service.domain.Feed;
 import com.VIG.mvc.service.domain.Payment;
+import com.VIG.mvc.service.domain.User;
 import com.VIG.mvc.service.feed.FeedServices;
 import com.VIG.mvc.service.payment.PaymentServices;
 
@@ -38,6 +41,7 @@ public class PaymentController {
 		System.out.println(productType+" : "+feedId);
 		ModelAndView mav = new ModelAndView();
 		
+		
 		//0: 프라임피드결제 , 1: 비즈니스전환결제, 2:후원결제
 		if(productType == 0) {
 			//프라임 피드 결제
@@ -54,9 +58,18 @@ public class PaymentController {
 		
 		if(productType == 2) {
 			//후원 결제
-			mav.setViewName("forward:/payment/addDonationView.jsp");
 			Feed feed = feedServices.getFeed(feedId);
+			User writer = feed.getWriter();
+			System.out.println(writer);
+			
+			if(writer.getRole().contains("business")) {
+			
+			mav.setViewName("forward:/payment/addDonationView.jsp");
+			
 			mav.addObject("feed", feed);
+			
+			}
+			
 		}
 		
 		return mav;
@@ -64,13 +77,19 @@ public class PaymentController {
 	}
 	
 	@RequestMapping(value="addDonation", method=RequestMethod.POST)
-	public ModelAndView addDonation(@ModelAttribute("payment") Payment payment) throws Exception {
+	public ModelAndView addDonation(@ModelAttribute("payment") Payment payment, HttpSession session) throws Exception {
 		
 		
 		System.out.println(payment);
+		User sessionUser = (User)session.getAttribute("user");
+		payment.setBuyer(sessionUser);
+		
+		
+		paymentServices.addPayment(payment);
+		
 		
 		ModelAndView mav = new ModelAndView();
-		mav.setViewName("forward:/payment/addDonation.jsp");
+		mav.setViewName("forward:/payment/addPayment.jsp");
 		mav.addObject("payment", payment);
 		
 		return mav;
