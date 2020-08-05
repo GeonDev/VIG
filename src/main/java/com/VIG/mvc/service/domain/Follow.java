ea3cc1358dcabb1dbb0bb228bf8f6aa6870091b5
Sooha <svha0504@gmail.com>
2020-08-05 20:59:03 +0900

    [수하]: 오류수정중

--- src/main/java/com/VIG/mvc/service/domain/Follow.java
+++ src/main/java/com/VIG/mvc/service/domain/Follow.java
@@ -2,19 +2,18 @@ package com.VIG.mvc.service.domain;
 
 import java.io.Serializable;
 import java.util.List;
-
 import lombok.Data;
 
 @Data
-public class Follow implements Serializable {
-		
+public class Follow implements Serializable{
+	
 	private int followId;
-	private User followed; //로그인한 유저
-	private User following; // 팔로우 혹은 언팔로우 할 유저
+	private User followingUser; //로그인한 유저
+	private User followedUser; // 팔로우 혹은 언팔로우 할 유저
 	private List<User> followers; //나를 팔로우한 유저
 	private List<User> followings; //내가 팔로우한 유저
-	private String FollowDate; //팔로우한 시간
-
+	private String followDate; //팔로우한 시간
+	
 	public Follow() {
 		// TODO Auto-generated constructor stub
 	}
