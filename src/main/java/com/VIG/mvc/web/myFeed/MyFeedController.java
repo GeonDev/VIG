fe75ad86f8019adea56c01321a6625f9ec0ad0b4
SonGeon <cadajue@gmail.com>
2020-08-11 15:46:14 +0900

    [손건] 비전 이미지 추출 부분 변경

--- src/main/java/com/VIG/mvc/web/myFeed/MyFeedController.java
+++ src/main/java/com/VIG/mvc/web/myFeed/MyFeedController.java
@@ -1,16 +1,21 @@
 package com.VIG.mvc.web.myFeed;
 
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.VIG.mvc.service.domain.Feed;
 import com.VIG.mvc.service.domain.User;
 import com.VIG.mvc.service.feed.FeedServices;
 import com.VIG.mvc.service.user.UserServices;
@@ -53,6 +58,19 @@ public class MyFeedController {
 		mav.setViewName("forward:/myFeed/getMyFeedList.jsp");
 		mav.addObject("writer", writer);
 		
+		
+		return mav; 
+	}
+	
+	
+	// Post 방식 전송 -> 피드 작성후에만 사용
+	@RequestMapping(value="getMyFeedList", method=RequestMethod.POST)
+	public ModelAndView getMyFeedList(HttpSession session) throws Exception {
+					
+		ModelAndView mav = new ModelAndView();	
+		
+		mav.setViewName("forward:/myFeed/getMyFeedList.jsp");
+		mav.addObject("writer", (User)session.getAttribute("user"));		
 		
 		return mav; 
 	}
