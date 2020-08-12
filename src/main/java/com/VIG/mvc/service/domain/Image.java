3c2aa28b86ca3228519509f0f64ab448405396aa
SonGeon <cadajue@gmail.com>
2020-08-12 14:52:32 +0900

    [손건] 미사용 필드값 제거/ 유저검색시 팔로우 버튼 삭제

--- src/main/java/com/VIG/mvc/service/domain/Image.java
+++ src/main/java/com/VIG/mvc/service/domain/Image.java
@@ -6,7 +6,7 @@ import java.util.List;
 import lombok.Data;
 
 @Data
-public class Image implements Serializable , Comparable<Image>{
+public class Image implements Serializable {
 
 	private int imageId;
 	private int feedId;
@@ -15,24 +15,11 @@ public class Image implements Serializable , Comparable<Image>{
 	private String imageFile;
 	private List<ImageColor> color;
 	private List<ImageKeyword> keyword;
-	
-	//최근 검색 키워드와 겹치는 개수 - 검색시 정렬용으로 사용
-	private int currentKeywordSameCount;
+
 	
 	public Image() {
 		// TODO Auto-generated constructor stub
 	}
 
-	@Override
-	public int compareTo(Image o) {
-		//내림차순 정렬		
-		if(this.currentKeywordSameCount > o.currentKeywordSameCount) {
-			return -1;
-		}else if(this.currentKeywordSameCount < o.currentKeywordSameCount) {
-			return 1;
-		}		
-		
-		return 0;
-	}
 
 }
