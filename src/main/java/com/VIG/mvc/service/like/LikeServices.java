cb519a879d4440f92df956f4cba78950bfaba271
SonGeon <geons.dev@gmail.com>
2020-08-14 20:09:17 +0900

    [손건] 좋아요를 받아오는 쿼리 추가

--- src/main/java/com/VIG/mvc/service/like/LikeServices.java
+++ src/main/java/com/VIG/mvc/service/like/LikeServices.java
@@ -20,5 +20,5 @@ public interface LikeServices {
 	//특정 월(date : '202008')에 특정 유저(userCode : 'user01')가 작성한 피드에 좋아요를 누른 수 - 손건
 	public HashMap<String,Object> getLikeMouthCount(HashMap<String, String> keys)throws Exception;
 	
-
+	public int getLikeCount (int feedId) throws Exception;  // 좋아요 수 받아오기
 }
