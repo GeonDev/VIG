8a6ce84128192cbd3efbee36086857c68773ee73
SonGeon <geons.dev@gmail.com>
2021-02-24 12:39:24 +0900

    웹소켓 헨들러 생성중/ 톰켓 설정 파일 정리

--- src/main/java/com/vig/VigApplication.java
+++ src/main/java/com/vig/VigApplication.java
@@ -4,9 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @EnableCaching
 @MapperScan(basePackageClasses = VigApplication.class)
@@ -14,10 +12,6 @@ import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 @PropertySource("classpath:common.properties")
 public class VigApplication {
 	
-    @Bean
-    public ServerEndpointExporter serverEndpointExporter() {
-        return new ServerEndpointExporter();
-    }
 	
 	public static void main(String[] args) {
 		SpringApplication.run(VigApplication.class, args);
