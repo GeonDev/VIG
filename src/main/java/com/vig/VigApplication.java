0c5064a4ca33a51e9fe7109bb422fd68a24afdea
SonGeon <guns716@kpu.ac.kr>
2021-03-02 18:14:14 +0900

    스케줄러 생성중

--- src/main/java/com/vig/VigApplication.java
+++ src/main/java/com/vig/VigApplication.java
@@ -5,8 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableCaching
+@EnableScheduling
 @MapperScan(basePackageClasses = VigApplication.class)
 @SpringBootApplication
 @PropertySource("classpath:common.properties")
