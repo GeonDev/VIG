e1197652f0e7e3bac613d389eba8f13f77b13f42
SonGeon <geons.dev@gmail.com>
2021-07-02 01:50:57 +0900

    설정 변경

--- SQL(script)/account_Local(MariaDB).sql
+++ SQL(script)/account_Local(MariaDB).sql
@@ -3,8 +3,9 @@ use mysql;
 create user 'WIN98'@localhost identified by 'WIN98';
 create user 'WIN98'@'%' identified by 'WIN98';
 
-grant all privileges on VIG.* to 'WIN98'@localhost identified by 'WIN98' ;
-grant all privileges on VIG.* to 'WIN98'@'%' identified by 'WIN98' ;
+GRANT USAGE ON *.* TO 'WIN98'@'localhost' IDENTIFIED BY PASSWORD 'WIN98';
+GRANT ALL PRIVILEGES ON 'vig'.* TO 'WIN98'@'localhost';
+
 
 SHOW GRANTS FOR 'WIN98'@'%';
 SHOW GRANTS FOR 'WIN98'@localhost;
